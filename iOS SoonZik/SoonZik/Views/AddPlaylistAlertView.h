9937fa49c0b64bd9363090bc52f1aa38ad23b712
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-02-02 11:22:56 -0800

    sort in the iOS project

---
+++ iOS SoonZik/SoonZik/Views/AddPlaylistAlertView.h
@@ -0,0 +1,13 @@
+//
+//  AddPlaylistAlertView.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 15/01/2015.
+//  Copyright (c) 2015 Coordina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPlaylistAlertView : UIAlertView
+
+@end
---
+++ iOS SoonZik/SoonZik/Views/AddPlaylistAlertView.m
@@ -0,0 +1,21 @@
+//
+//  AddPlaylistAlertView.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 15/01/2015.
+//  Copyright (c) 2015 Coordina. All rights reserved.
+//
+
+#import "AddPlaylistAlertView.h"
+
+@implementation AddPlaylistAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
