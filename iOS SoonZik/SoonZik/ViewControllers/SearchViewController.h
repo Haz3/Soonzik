6ecc5dfe133023275bcd02a14b460222b7df729b
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-02-23 17:10:25 -0800

    iOS : Menu modified

---
+++ iOS SoonZik/SoonZik/ViewControllers/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 23/02/15.
+//  Copyright (c) 2015 Coordina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
