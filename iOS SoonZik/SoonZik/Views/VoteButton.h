770e3aeec1512318d27853c1ebac77049800773f
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-03-04 18:23:50 -0800

    iOS : Modification of The Player (becomes scrollable)

---
+++ iOS SoonZik/SoonZik/Views/VoteButton.h
@@ -0,0 +1,13 @@
+//
+//  VoteButton.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 02/03/15.
+//  Copyright (c) 2015 Coordina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VoteButton : UIButton
+
+@end
---
+++ iOS SoonZik/SoonZik/Views/VoteButton.m
@@ -0,0 +1,21 @@
+//
+//  VoteButton.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 02/03/15.
+//  Copyright (c) 2015 Coordina. All rights reserved.
+//
+
+#import "VoteButton.h"
+
+@implementation VoteButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
