e3cacb6fb96c8d64ae09b500943b9a7f1b042d89
Florian Dewulf <floriandewulf@MacBook-Air.local>
2015-03-25 17:14:50 -0400

    Ca commence à me casser les couilles

--- RailsProject/config/boot.rb
+++ RailsProject/config/boot.rb
@@ -1,3 +1,5 @@
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
