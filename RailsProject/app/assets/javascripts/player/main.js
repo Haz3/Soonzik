6694f2f6d4aa6feb099810f0571ffe27b526f429
Maxime Wojciak <wojciak.maxime@gmail.com>
2015-03-26 19:07:39 -0600

    commit player foireux

---
+++ RailsProject/app/assets/javascripts/player/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+$(document).ready(function() {
+
+    // Create an instance
+    var wavesurfer = Object.create(WaveSurfer);
+
+    // Init & load audio file
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log("Je suis au dessus de l'init");
+        // Init
+        wavesurfer.init({
+            container: document.querySelector('#waveform'),
+            minPxPerSec: 50,
+            scrollParent: false,
+            waveColor: '#d3d7da',
+            progressColor: '#40a8e2'
+        });
+
+        // Load audio from URL
+        wavesurfer.load('http://maxime.wojciak.fr/02%20Teardrop%20%28Mad%20Professor%20Mazaruni%20Mix%29.mp3');
+
+
+        // Log errors
+        wavesurfer.on('error', function (msg) {
+            console.log(msg);
+        });
+
+        // Bind play/pause button
+        if (document.querySelector('[data-action="play"]').addEventListener('click', wavesurfer.playPause.bind(wavesurfer))) {
+            console.log("coucou");
+        }
+
+
+        // Progress bar
+        (function () {
+            var progressDiv = document.querySelector('#progress-bar');
+            var progressBar = progressDiv.querySelector('.progress-bar');
+
+            var showProgress = function (percent) {
+                progressDiv.style.display = 'block';
+                progressBar.style.width = percent + '%';
+            };
+
+            var hideProgress = function () {
+                progressDiv.style.display = 'none';
+            };
+
+            wavesurfer.on('loading', showProgress);
+            wavesurfer.on('ready', hideProgress);
+            wavesurfer.on('destroy', hideProgress);
+            wavesurfer.on('error', hideProgress);
+        }());
+    });
+});
