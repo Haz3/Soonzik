bec9abe56d37bb2f5cf4cb6693c26843b3f4d46a
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-04-02 20:19:36 -0700

    Graphics modified

---
+++ iOS SoonZik/SoonZik/Views/FriendCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  FriendCollectionViewCell.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 02/04/15.
+//  Copyright (c) 2015 Coordina. All rights reserved.
+//
+
+#import "FriendCollectionViewCell.h"
+
+@implementation FriendCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
