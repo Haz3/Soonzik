5177948565ecf9689f433b366c2698aadeecd63a
Maxime Wojciak <wojciak.maxime@gmail.com>
2015-04-04 14:01:42 -0600

    Commit avant formatage

--- RailsProject/app/helpers/application_helper.rb
+++ RailsProject/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def socialImgConnect(provider)
+ 		case provider
+   			when "Facebook"
+     			return "step fi-social-facebook size-72"
+   			when "Twitter"
+   				return "step fi-social-twitter size-72"
+   			when "Google"
+   				return "step fi-social-google-plus size-72"
+ 		end
+	end
 end
