71c3ee7c4520c4821a9758d2844a9e2c572ff4b2
Florian Dewulf <floriandewulf@MacBook-Air.local>
2015-04-09 10:31:17 -0400

    Bug of Pix' fixed

--- RailsProject/app/helpers/application_helper.rb
+++ RailsProject/app/helpers/application_helper.rb
@@ -1,13 +1,15 @@
 module ApplicationHelper
 
-	def socialImgConnect(provider)
+	def socialIconConnect(provider)
  		case provider
-   			when "Facebook"
-     			return "step fi-social-facebook size-72"
-   			when "Twitter"
-   				return "step fi-social-twitter size-72"
-   			when "Google"
-   				return "step fi-social-google-plus size-72"
+   			when "facebook"
+     			return "<i class='step fi-social-facebook size-42'></i>"
+   			when "twitter"
+   				return "<i class='step fi-social-twitter size-42'></i>"
+   			when "google"
+   				return "<i class='step fi-social-google-plus size-42'></i>"
+   			else
+   				return "<span>#{provider}</span>"
  		end
 	end
 end
