c48c75cf583a079c5e30348c2e71a01e2402611f
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-05-04 17:04:41 -0700

    Login Facebook + listening view + explore part

---
+++ iOS SoonZik/SoonZik/ViewControllers/CurrentListViewController.m
@@ -0,0 +1,37 @@
+//
+//  CurrentListViewController.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 02/05/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "CurrentListViewController.h"
+
+@interface CurrentListViewController ()
+
+@end
+
+@implementation CurrentListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
