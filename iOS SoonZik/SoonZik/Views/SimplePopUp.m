81e0f398d5035bb57a2cd98350051c98fd6eaaa2
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-05-15 16:08:04 -0700

    New pop up system

---
+++ iOS SoonZik/SoonZik/Views/SimplePopUp.m
@@ -0,0 +1,21 @@
+//
+//  SimplePopUp.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 15/05/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "SimplePopUp.h"
+
+@implementation SimplePopUp
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
