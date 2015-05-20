78ee6bf9b65f892bd685a4a5b106d64d880f95ac
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-05-19 18:14:01 -0700

    New view for the news detail

---
+++ iOS SoonZik/SoonZik/Classes/NewsText.h
@@ -0,0 +1,13 @@
+//
+//  NewsText.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 19/05/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsText : NSObject
+
+@end
---
+++ iOS SoonZik/SoonZik/ViewControllers/DetailNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailNewsViewController.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 19/05/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailNewsViewController : UITableViewController
+
+@end
