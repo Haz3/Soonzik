3c57cc50d9fc44b8f0726ce44690943dc533450f
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-05-23 18:11:09 -0700

    Pop up realised for social actions like Facebook sharing or Twitter sharing. Only available for news sharing for the moment.

---
+++ iOS SoonZik/SoonZik/Views/ShareView.h
@@ -0,0 +1,13 @@
+//
+//  ShareView.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 23/05/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareView : UIView
+
+@end
---
+++ iOS SoonZik/SoonZik/Views/ShareView.m
@@ -0,0 +1,21 @@
+//
+//  ShareView.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 23/05/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "ShareView.h"
+
+@implementation ShareView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
