5535dc9dbff79fbb2ac14c6381fbfa773a592fb2
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-06-11 16:05:41 -0700

    Explore section changed -> Addition of influence and genre then. -> Addition of suggested artists

---
+++ iOS SoonZik/SoonZik/ViewControllers/InfluenceViewController.m
@@ -0,0 +1,37 @@
+//
+//  InfluenceViewController.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 11/06/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "InfluenceViewController.h"
+
+@interface InfluenceViewController ()
+
+@end
+
+@implementation InfluenceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
