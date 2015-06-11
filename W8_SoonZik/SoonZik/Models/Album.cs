af06ba19963de0cf25f4313892cdb29c77b3be14
rodrig_h <julien.rodrigues@epitech.eu>
2015-06-11 13:21:22 +0200

    Windows 8 - first push

---
+++ W8_SoonZik/SoonZik/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoonZik.Models
+{
+    public class Album
+    {
+        public int id { get; set; }
+        public User artist { get; set; }
+        public string title { get; set; }
+        public Genre genre { get; set; }
+        public double price { get; set; }
+        public int year { get; set; }
+        public List<Music> music_list { get; set; }
+    }
+}
---
+++ W8_SoonZik/SoonZik/Models/Battle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoonZik.Models
+{
+    public class Battle
+    {
+        public int id { get; set; }
+        public DateTime date_start { get; set; }
+        public DateTime date_end { get; set; }
+        public User artist1 { get; set; }
+        public User artist2 { get; set; }
+        public float vote_artist1 { get; set; }
+        public float vote_artist2 { get; set; }
+    }
+}
