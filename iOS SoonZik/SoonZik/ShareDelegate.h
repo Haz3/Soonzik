ebefe1f871f90643c06ba12cb0c071727f1d7145
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-06-11 21:39:52 -0700

    Clean methods and Classes : avoid redundancies Sharing news and artists on social networks

---
+++ iOS SoonZik/SoonZik/ShareDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ShareDelegate.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 11/06/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#ifndef SoonZik_ShareDelegate_h
+#define SoonZik_ShareDelegate_h
+
+
+#endif
