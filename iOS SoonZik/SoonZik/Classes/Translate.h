ea2ac58ca15d647c53bc4236915651eb23fa3828
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-06-21 16:20:58 -0700

    English translation done and used in the app

---
+++ iOS SoonZik/SoonZik/Classes/Translate.h
@@ -0,0 +1,13 @@
+//
+//  Translate.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 21/06/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Translate : NSObject
+
+@end
