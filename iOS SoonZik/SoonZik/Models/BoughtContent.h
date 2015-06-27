f93de9c78fbac37900a90a62c6c5a52ec3ee5f76
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-06-26 18:23:34 -0700

    Battle + List of battles + Modification of Search + get all the content the user bought

---
+++ iOS SoonZik/SoonZik/Models/BoughtContent.h
@@ -0,0 +1,13 @@
+//
+//  BoughtContent.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 26/06/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "ObjectFactory.h"
+
+@interface BoughtContent : ObjectFactory
+
+@end
---
+++ iOS SoonZik/SoonZik/Models/BoughtContent.m
@@ -0,0 +1,13 @@
+//
+//  BoughtContent.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 26/06/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "BoughtContent.h"
+
+@implementation BoughtContent
+
+@end
