f10ce0023d5c149028525af1163c9f3616622086
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-07-06 11:27:34 -0700

    All kind of bought content

---
+++ iOS SoonZik/SoonZik/Views/MusicTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MusicTableViewCell.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 06/07/15.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "MusicTableViewCell.h"
+
+@implementation MusicTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
