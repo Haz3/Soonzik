804f1eceb44ab193a4101ef0e00e1add9969654e
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-08-18 19:14:38 +0200

    Cart view done / add item to cart done / remove item of cart done / need to finish the gift to a friend

---
+++ iOS SoonZik/SoonZik/Classes/CartDeleteButton.m
@@ -0,0 +1,21 @@
+//
+//  CartDeleteButton.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 18/08/2015.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "CartDeleteButton.h"
+
+@implementation CartDeleteButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ iOS SoonZik/SoonZik/Views/CartItemTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CartItemTableViewCell.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 18/08/2015.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "CartItemTableViewCell.h"
+
+@implementation CartItemTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
