b550d3e14c2fdd87587f1453b314709bfb43fdc0
Maxime Wojciak <Haz3@hazbook-pro.home>
2015-08-19 17:39:26 +0200

    pagination of news is now working

--- RailsProject/app/assets/javascripts/AngularJS/controllers/NewsController.js
+++ RailsProject/app/assets/javascripts/AngularJS/controllers/NewsController.js
@@ -2,6 +2,10 @@ SoonzikApp.controller('NewsCtrl', ['$scope', '$routeParams', 'SecureAuth', 'HTTP
 
 	$scope.loading = true;
 
+	$scope.news = [];
+	$scope.newsLoading = false;
+	$scope.newsOffset = 0;
+
 	$scope.commentariesOffset = 0;
 	$scope.commentaries = [];
 	$scope.commentLoading = false;
@@ -9,21 +13,38 @@ SoonzikApp.controller('NewsCtrl', ['$scope', '$routeParams', 'SecureAuth', 'HTTP
 		content : ""
 	};
 
-
 	$scope.initFoundation = function () {
 		$(document).foundation();
+
+	}
+
+	$scope.initNews = function() {
+		$scope.initFoundation();
+		
+		$(window).scroll(function() {
+			if($(window).scrollTop() + $(window).height() == $(document).height() && $scope.newsLoading == false) {
+				$scope.$apply(function() {
+					$scope.newsLoading = true;
+					$scope.showNews();
+				});
+			}
+		});
 	}
 
 	$scope.showNews = function() {
 		var parameters = [
-			{ key: "order_by_asc[]", value: "date"}
+			{ key: "order_by_desc[]", value: "date"},
+  			{ key: "offset", value: $scope.newsOffset },
+  			{ key: "limit", value: 20 }
 		];
 
 		$scope.News = true;
 
-		HTTPService.findNews(parameters).then(function(news) {
+		HTTPService.findNews(parameters).then(function(response) {
 			
-			$scope.news = news.data.content;
+			$scope.news = $scope.news.concat(response.data.content);
+			$scope.newsOffset = $scope.news.length;
+			$scope.newsLoading = false;
 
 		}, function (error) {
 			console.log("No News Available");
@@ -39,7 +60,7 @@ SoonzikApp.controller('NewsCtrl', ['$scope', '$routeParams', 'SecureAuth', 'HTTP
 
 		HTTPService.showNews(newsId).then(function(response) {
 
-			$scope.thisNews = response.data.content;
+			$scope.thisNews = response.concat.data.content;
 			$scope.attachments = $scope.thisNews.attachments;
 			$scope.author = $scope.thisNews.user;
 
