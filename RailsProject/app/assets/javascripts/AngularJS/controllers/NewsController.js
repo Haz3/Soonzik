655e689bcd1e942e438cee868fac8641d0a01f52
Maxime Wojciak <Haz3@hazbook-pro.home>
2015-08-19 15:03:25 +0200

    Comment is now add automaticaly

--- RailsProject/app/assets/javascripts/AngularJS/controllers/NewsController.js
+++ RailsProject/app/assets/javascripts/AngularJS/controllers/NewsController.js
@@ -1,7 +1,12 @@
 SoonzikApp.controller('NewsCtrl', ['$scope', '$routeParams', 'SecureAuth', 'HTTPService', function ($scope, $routeParams, SecureAuth, HTTPService) {
 
 	$scope.loading = true;
+
+	$scope.commentariesOffset = 0;
+	$scope.commentaries = [];
+	$scope.commentLoading = false;
 	$scope.commentary = { content: "" }
+
 
 	$scope.initFoundation = function () {
 		$(document).foundation();
@@ -40,15 +45,38 @@ SoonzikApp.controller('NewsCtrl', ['$scope', '$routeParams', 'SecureAuth', 'HTTP
 			console.log("This news doesn't exist");
 		});
 
-		HTTPService.showComment(newsId, parameters).then(function(response) {
-			$scope.comments = response.data.content;
-
-		}, function (error) {
-			console.log("This news doesn't exist");
+		$(window).scroll(function() {
+			if($(window).scrollTop() + $(window).height() == $(document).height() && $scope.commentLoading == false) {
+				$scope.$apply(function() {
+					$scope.commentLoading = true;
+					$scope.showComments();
+				});
+			}
 		});
 
 		$scope.thisNewsId = true;
 
+	}
+
+	$scope.showComments = function() {
+
+		var parameters = [
+  			{ key: "offset", value: $scope.commentariesOffset },
+  			{ key: "limit", value: 20 },
+//  			{ order_reverse: false }
+  		];
+
+  		var newsId = $routeParams.id;
+		
+		HTTPService.showComment(newsId, parameters).then(function(response) {
+
+			$scope.commentaries = $scope.commentaries.concat(response.data.content);
+			$scope.commentariesOffset = $scope.commentaries.length;
+			$scope.commentLoading = false;
+  		
+  		}, function(error) {
+  			NotificationService.error("Error while loading commentaries");
+  		});
 	}
 
 	$scope.postComment = function() {
@@ -56,18 +84,20 @@ SoonzikApp.controller('NewsCtrl', ['$scope', '$routeParams', 'SecureAuth', 'HTTP
 			var parameters =
 		  		{	secureKey: key,
 			  		user_id: id,
-			  		content: $scope.commentary.content };
+			  		content: $scope.commentary.content
+			  	};
 
-			console.log(parameters);
-
-			HTTPService.addComment($routeParams.id, parameters).then(function(data) {
-				parameters.content = $scope.commentary.content;
+			HTTPService.addComment($routeParams.id, parameters).then(function(response) {
+				
+				$scope.commentaries.push(response.data.content);
+				$scope.commentariesOffset++;
+				$scope.commentary.content = "";
 
 			}, function(error) {
-				console.log("erreur HTTPService");
+				NotificationService.error("Error while saving your comment, please try later");
 			});
 		}, function(error) {
-			console.log("erreur securedTransaction");
+			NotificationService.error("Error while saving your comment, please try later");
 		});
 	}
 
