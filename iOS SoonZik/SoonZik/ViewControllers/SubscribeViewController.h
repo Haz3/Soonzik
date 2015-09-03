4563ef3df569ee929a9ab6920d33a2b72b676222
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-09-03 11:33:29 +0200

    Subscribe done

---
+++ iOS SoonZik/SoonZik/ViewControllers/SubscribeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubscribeViewController.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 03/09/2015.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubscribeViewController : UIViewController
+
+@end
