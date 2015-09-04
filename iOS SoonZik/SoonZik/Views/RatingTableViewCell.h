0fb285d894b646118b684bcade326c56374fa25b
Maxime Sauvage <maxime.sauvage.14@gmail.com>
2015-09-04 12:25:41 +0200

    Rating on music Done

---
+++ iOS SoonZik/SoonZik/Views/RatingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RatingTableViewCell.h
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 04/09/2015.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RatingTableViewCell : UITableViewCell
+
+@end
---
+++ iOS SoonZik/SoonZik/Views/RatingTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RatingTableViewCell.m
+//  SoonZik
+//
+//  Created by Maxime Sauvage on 04/09/2015.
+//  Copyright (c) 2015 SoonZik - Maxime SAUVAGE. All rights reserved.
+//
+
+#import "RatingTableViewCell.h"
+
+@implementation RatingTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
